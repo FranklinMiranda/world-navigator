7f3cf50e54e37e5519246732687d4163f2d9520a
FranklinMiranda <fdm61344@gmail.com>
2023-01-03 15:51:07 -0500

    CM

--- world-navigator/src/Components/App.js
+++ world-navigator/src/Components/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Data from './Data';
+
+const BASE_URL = 'https://restcountries.com/v3.1/all';
+
 
 function App() {
   return (
--- world-navigator/src/Components/Data.js
+++ world-navigator/src/Components/Data.js
@@ -1,23 +1,3 @@
-import React, { useState, useEffect } from 'react';
+
 import axios from 'axios';
 
-const BASE_URL = 'https://restcountries.com/v3.1/all';
-
-function Data() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    const loadRes = async () => {
-      const res = await axios.get(BASE_URL);
-
-      setCountries(res.data);
-    };
-    loadRes();
-  }, []);
-
-  console.log(countries);
-
-  return <div className="Data"></div>;
-}
-
-export default Data;
