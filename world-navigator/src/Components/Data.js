e5940e0f2f08bb2ea73d05113a87b5f0c2d1b2f0
FranklinMiranda <fdm61344@gmail.com>
2023-01-03 14:01:49 -0500

    CM

--- world-navigator/src/Components/Data.js
+++ world-navigator/src/Components/Data.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from '@mui/material/Card';
+import { CardContent, Typography } from '@mui/material';
 
 const BASE_URL = 'https://restcountries.com/v3.1/all';
 
@@ -20,7 +21,17 @@ function Data() {
   return (
     <div className="Data">
       {countries.map((item) => {
-        return <Card variant="outlined">{item.name.common}</Card>;
+        return (
+          <Card variant="outlined">
+            <CardContent>
+              <Typography>Name: {item.name.common}</Typography>
+              <Typography>Capital: {item.capital}</Typography>
+              <Typography>Population: {item.population}</Typography>
+              <Typography>Region: {item.region}</Typography>
+              <Typography>Sub-Region: {item.subregion}</Typography>
+            </CardContent>
+          </Card>
+        );
       })}
     </div>
   );
