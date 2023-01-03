352a2ac7c115360aa24b0df4d2455a2db1a139d4
FranklinMiranda <fdm61344@gmail.com>
2023-01-03 17:00:27 -0500

    CM

--- world-navigator/src/Components/App.js
+++ world-navigator/src/Components/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import NavBar from './NavBar';
 
 const BASE_URL = 'https://restcountries.com/v3.1/all?fields=name,capital,languages,region,subregion,population';
 
@@ -10,23 +9,12 @@ function App() {
   useEffect(() => {
     const loadRes = async () => {
       const res = await axios.get(BASE_URL);
-
-      console.log(res.data);
       setCountries(res.data);
     };
-
     loadRes();
   }, []);
 
-let region = countries.map((item) => {return item.region})
-region = [...new Set(region)]
-console.log(region)
-
-  return (
-    <div className="App">
-      <NavBar region={region} />
-    </div>
-  );
+  console.log('Countries', countries);
 }
 
 export default App;
